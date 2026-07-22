6172ce8816b11b49efd38536d04d06f0df8e7650
HaikunZhang1 <zhk1990119@126.com>
2026-07-22 11:14:02 -0500

    Add other functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
+
+def main(input_file, output_file, graph_file):
+    print("--START--")
+
+    # Read the data from JSON file
+    eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
+
+    # Convert and export data to CSV file
+    write_dataframe_to_csv(eva_data, output_file)
+
+    # Sort dataframe by date ready to be plotted (date values are on x-axis)
+    eva_data.sort_values('date', inplace=True)
+
+    # Plot cumulative time spent in space over years
+    plot_cumulative_time_in_space(eva_data, graph_file)
+
+    print("--END--")
+
+... 
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 def main(input_file, output_file, graph_file):
     print("--START--")
@@ -94,7 +149,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/60
+    duration_hours = int(hours) + int(minutes)/60  
     return duration_hours
 
 
@@ -128,4 +183,3 @@ if __name__ == "__main__":
     graph_file = './cumulative_eva_graph.png'
 
     main(input_file, output_file, graph_file)
-    
--- test_code.py
+++ test_code.py
@@ -1,5 +1,8 @@
 import pytest
-from eva_data_analysis import text_to_duration
+from eva_data_analysis import (
+    text_to_duration,
+    calculate_crew_size
+)
 
 def test_text_to_duration_integer():
     assert text_to_duration("10:00") == 10            # assert: compare the real and expected values, if they are not equal, the test will fail
@@ -17,7 +20,18 @@ def test_text_to_duration_irrational():
     # assert abs(text_to_duration("10:20") - 10.3333333) < 1e-5
     assert text_to_duration("10:20") == pytest.approx(10.3333333)
 
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("Valentina Tereshkova;", 1),
+    ("Judith Resnik; Sally Ride;", 2)
+])
+def test_calculate_crew_size(input_value, expected_result):
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
+
 
 # test_text_to_duration_integer()
 # test_text_to_duration_float()
 # test_text_to_duration_irrational()
+
+
+
