e964657c5148205996944f85d2529755033392d0
HaikunZhang1 <zhk1990119@126.com>
2026-07-22 10:31:26 -0500

    Moved test code to use pytest

--- test_code.py
+++ test_code.py
@@ -1,3 +1,4 @@
+import pytest
 from eva_data_analysis import text_to_duration
 
 def test_text_to_duration_integer():
@@ -10,11 +11,13 @@ def test_text_to_duration_float():
     assert text_to_duration("10:15") == 10.25
 
 def test_text_to_duration_irrational():
-    print(1e-5)
-    print(abs(text_to_duration("10:20")))
-    print(abs(text_to_duration("10:20") - 10.3333333))
-    assert abs(text_to_duration("10:20") - 10.3333333) < 1e-5
+    # print(1e-5)
+    # print(abs(text_to_duration("10:20")))
+    # print(abs(text_to_duration("10:20") - 10.3333333))
+    # assert abs(text_to_duration("10:20") - 10.3333333) < 1e-5
+    assert text_to_duration("10:20") == pytest.approx(10.3333333)
 
-test_text_to_duration_integer()
-test_text_to_duration_float()
-test_text_to_duration_irrational()
+
+# test_text_to_duration_integer()
+# test_text_to_duration_float()
+# test_text_to_duration_irrational()
