b66d55e3debaace7a03689ec90794dee40d47bae
HaikunZhang1 <zhk1990119@126.com>
2026-07-23 11:27:23 -0500

    Add new comment

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,61 +4,7 @@ import sys
 import re
 
 
-def main(input_file, output_file, graph_file):
-    print("--START--")
-
-    # Read the data from JSON file
-    eva_data = read_json_to_dataframe(input_file)
-
-    # Calculate and add crew size to data
-    eva_data = add_crew_size_column(eva_data) # added this line
-
-    # Convert and export data to CSV file
-    write_dataframe_to_csv(eva_data, output_file)
-
-    # Sort dataframe by date ready to be plotted (date values are on x-axis)
-    eva_data.sort_values('date', inplace=True)
-
-    # Plot cumulative time spent in space over years
-    plot_cumulative_time_in_space(eva_data, graph_file)
-
-    print("--END--")
-
-... 
-
-def calculate_crew_size(crew):
-    """
-    Calculate the size of the crew for a single crew entry
-
-    Args:
-        crew (str): The text entry in the crew column containing a list of crew member names
-
-    Returns:
-        (int): The crew size
-    """
-    if crew.split() == []:
-        return None
-    else:
-        return len(re.split(r';', crew))-1
-
-def add_crew_size_column(df):
-    """
-    Add crew_size column to the dataset containing the value of the crew size
-
-    Args:
-        df (pd.DataFrame): The input data frame.
-
-    Returns:
-        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
-    """
-    print('Adding crew size variable (crew_size) to dataset')
-    df_copy = df.copy()
-    df_copy["crew_size"] = df_copy["crew"].apply(
-        calculate_crew_size
-    )
-    return df_copy
-
-def main(input_file, output_file, graph_file):
+def main(input_file, output_file, duration_by_astronaut_output_file, graph_file):
     print("--START--")
 
     # Read the data from JSON file
@@ -66,6 +12,11 @@ def main(input_file, output_file, graph_file):
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
+
+    # Calculate summary table for total EVA per astronaut
+    duration_by_astronaut_df = summary_duration_by_astronaut(eva_data)
+    # Save summary duration data by each astronaut to CSV file
+    write_dataframe_to_csv(duration_by_astronaut_df, duration_by_astronaut_output_file)
 
     # Sort dataframe by date ready to be plotted (date values are on x-axis)
     eva_data.sort_values('date', inplace=True)
@@ -85,7 +36,7 @@ def read_json_to_dataframe(input_file):
         input_file (file or str): The file object or path to the JSON file.
 
     Returns:
-         eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
+         eva_df (pd.DataFrame): The cleaned data as a dataframe structure
     """
     print(f'Reading JSON file {input_file}')
     # Read the data from a JSON file into a Pandas dataframe
@@ -110,6 +61,7 @@ def write_dataframe_to_csv(df, output_file):
     print(f'Saving to CSV file {output_file}')
     # Save dataframe to CSV file for later analysis
     df.to_csv(output_file, index=False, encoding='utf-8')
+
 
 def plot_cumulative_time_in_space(df, graph_file):
     """
@@ -149,7 +101,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/60  
+    duration_hours = int(hours) + int(minutes)/60
     return duration_hours
 
 
@@ -168,7 +120,41 @@ def add_duration_hours(df):
         text_to_duration
     )
     return df_copy
-    
+
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
+
+
 def summary_duration_by_astronaut(df):
     """
     Summarise the duration data by each astronaut and saves resulting table to a CSV file
@@ -184,21 +170,23 @@ def summary_duration_by_astronaut(df):
     subset = df.loc[:,['crew', 'duration']] # subset to work with only relevant columns
     subset = add_duration_hours(subset) # need duration_hours for easier calcs
     subset = subset.drop('duration', axis=1) # dropping the extra 'duration' column as it contains string values not suitable for calculations
-    subset = subset.groupby('crew').sum()
+    subset = subset.groupby('crew').sum() 
     subset = subset.reset_index() # make group index a column in the dataframe
     return subset
 
+# when is __name__ != __main__?
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        input_file = 'eva-data.json'
+        output_file = 'eva-data.csv'
         print(f'Using default input and output filenames')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    graph_file = 'cumulative_eva_graph.png'
+    duration_by_astronaut_output_file = 'duration_by_astronaut.csv'
 
-    main(input_file, output_file, graph_file)
+    main(input_file, output_file, duration_by_astronaut_output_file, graph_file)
